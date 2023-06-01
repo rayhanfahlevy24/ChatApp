fd318f55455af8c5c86f55b86f18d12179da4fc6
Rayhan Fahlevy <rayhanfahlevy@gmail.com>
2023-06-01 15:04:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/splash.java
@@ -0,0 +1,45 @@
+package com.example.chatapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splash extends AppCompatActivity {
+    ImageView logo;
+    TextView name;
+    Animation topAnim, bottomAnim;
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        logo = findViewById(R.id.logoimg);
+        name = findViewById(R.id.logonameimg);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo.setAnimation(topAnim);
+        name.setAnimation(bottomAnim);
+
+      new Handler().postDelayed(new Runnable() {
+          @Override
+          public void run() {
+              Intent intent = new Intent(splash.this,MainActivity.class);
+              startActivity(intent);
+              finish();
+          }
+      },4000);
+    }
+}
